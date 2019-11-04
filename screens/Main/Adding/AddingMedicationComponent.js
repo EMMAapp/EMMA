0eeb493507934f51a5a31091f88edb2ec97f1bdf
ReallyLiri <reallyliri@gmail.com>
2019-11-04 20:18:26 +0200

    extracted autocomplete component

--- screens/Main/Adding/AddingMedicationComponent.js
+++ screens/Main/Adding/AddingMedicationComponent.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {StyleSheet, Text, TextInput, View, Button, TouchableOpacity} from 'react-native'
 import localization from "../../../utils/localization";
-import Autocomplete from "react-native-autocomplete-input";
+import Autocomplete from "../../../components/Autocomplete";
 import Medications from '../../../constants/Medications';
 
 export default function AddingMedicationComponent() {
@@ -13,39 +13,14 @@ export default function AddingMedicationComponent() {
     const [eventsAndReminders, setEventsAndReminders] = useState([]);
     const [note, setNote] = useState('');
 
-    const [query, setQuery] = useState('');
-    const [hideAutoComplete, setHideAutoComplete] = useState(true);
-
-    const filteredMedications = Medications.filter(
-        medication => medication.aliases.some(
-            alias => alias.toLowerCase().includes(query)
-        )
-    );
-
     return (
         <View style={styles.container}>
             <View style={styles.autocompleteContainer}>
                 <Text style={{color: '#e93766'}}>{localization('drugOrSupplement')}</Text>
                 <Autocomplete
-                    data={filteredMedications.map(medication => medication.title)}
-                    defaultValue={medication?.title}
-                    onChangeText={text => setQuery(text.toLowerCase())}
-                    renderItem={({ item, i }) => (
-                        <TouchableOpacity onPress={() => {
-                            setQuery('');
-                            setMedication(Medications.filter(medication => medication.title === item)[0]);
-                            setHideAutoComplete(true);
-                        }}>
-                            <Text>{item}</Text>
-                        </TouchableOpacity>
-                    )}
-                    hideResults={hideAutoComplete}
-                    onBlur={() => {
-                        setHideAutoComplete(true);
-                    }}
-                    onFocus={() => {
-                        setHideAutoComplete(false);
-                    }}
+                    data={Medications}
+                    selectedItem={medication}
+                    setSelectedItem={setMedication}
                 />
                 <TextInput
                     style={{paddingTop: 100}}

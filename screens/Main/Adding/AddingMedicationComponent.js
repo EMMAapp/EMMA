5d477bc998d4ae7542793468c4af253d9ec43768
ReallyLiri <reallyliri@gmail.com>
2019-11-07 19:29:13 +0200

    Move to file

--- screens/Main/Adding/AddingMedicationComponent.js
+++ screens/Main/Adding/AddingMedicationComponent.js
@@ -5,40 +5,10 @@ import Autocomplete from "../../../components/Autocomplete";
 import Medications from '../../../constants/Medications';
 import NumericInput from "../../../components/NumericInput";
 import Calendar from "../../../components/Calendar";
-import DayTimeInput from "../../../components/DayTimeInput";
 import {CALENDAR} from "../../../navigation/Routes";
 import id from "../../../utils/id";
 import {store, syncPatientData} from "../../../store";
-
-const EventAndReminderPicker = ({eventAndReminder, setEventAndReminder}) => {
-
-    const {event, reminder, reminderDisabled} = eventAndReminder;
-
-    return (
-        <View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text style={{color: '#e93766', flex: 1}}>{localization('timeOfEvent')}</Text>
-                <DayTimeInput
-                    style={{flex: 1}}
-                    dayTime={event}
-                    setDayTime={dayTime => setEventAndReminder({...eventAndReminder, event: dayTime})}
-                    disabled={false}
-                />
-                <Text style={{color: '#e93766', flex: 1}}>{localization('reminder')}</Text>
-                <DayTimeInput
-                    style={{flex: 1}}
-                    dayTime={reminder}
-                    setDayTime={dayTime => setEventAndReminder({...eventAndReminder, reminder: dayTime})}
-                    disabled={reminderDisabled}
-                />
-                <Switch style={{flex: 1}}
-                        value={!reminderDisabled}
-                        onValueChange={(enabled) => setEventAndReminder({...eventAndReminder, reminderDisabled: !enabled})}
-                />
-            </View>
-        </View>
-    )
-};
+import EventDetailsPicker from "./EventDetailsPicker";
 
 const initialState = {
     id: null,
@@ -144,7 +114,7 @@ export default function AddingMedicationComponent({navigation}) {
                 {
                     [...Array(state.timesPerDay).keys()].map(i => {
                         return (
-                            <EventAndReminderPicker
+                            <EventDetailsPicker
                                 key={i}
                                 eventAndReminder={state.eventsAndReminders[i]}
                                 setEventAndReminder={(eventAndReminder) => setEventsAndReminder(eventAndReminder, i)}

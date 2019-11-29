a21da5c14c4787d2270963c445b6658f98d4d386
ReallyLiri <reallyliri@gmail.com>
2019-11-29 09:53:09 +0200

    motorcycle

---
+++ components/Divider.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import {View} from "react-native";
+import Colors from "../constants/Colors";
+import {marginStyle} from "../constants/Styles";
+
+const StyledView = styled(View)`
+border-width: 0.5px;
+border-color: ${props => props.color || Colors.gray};
+opacity: 0.5;
+`;
+
+export default ({color}) =>
+    <StyledView color={color} style={[
+        marginStyle(10, 'top'),
+        marginStyle(10, 'bottom')
+    ]}/>;

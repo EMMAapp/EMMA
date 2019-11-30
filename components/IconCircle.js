42df9ed178393f8dcb09c608ed2a637b11b15f72
ReallyLiri <reallyliri@gmail.com>
2019-11-30 19:22:33 +0200

    edit event

--- components/IconCircle.js
+++
@@ -1,9 +0,0 @@
-import Icon from "./Icon";
-import {View} from "react-native";
-import React from "react";
-import {marginStyle, paddingStyle} from "../constants/Styles";
-
-export default ({name, backgroundColor, iconColor}) =>
-    <View style={[{borderRadius: '50%', backgroundColor}, marginStyle(1), paddingStyle(1)]}>
-        <Icon name={name} color={iconColor}/>
-    </View>;

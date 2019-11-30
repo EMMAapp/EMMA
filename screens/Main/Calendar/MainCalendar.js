aabe1fc4a4aafafdf9e6d87f81a31e235c7040eb
ReallyLiri <reallyliri@gmail.com>
2019-11-30 19:17:48 +0200

    no clear cache

--- screens/Main/Calendar/MainCalendar.js
+++ screens/Main/Calendar/MainCalendar.js
@@ -12,22 +12,16 @@ export default function CalendarTab({
     setSelectedDay,
     markedDates,
     dayRender,
-    clearDayFromCache
 }) {
 
     const currentMarkedDates = {...markedDates};
     pushByMapKey(currentMarkedDates, selectedDay, selectedDayColoring);
 
-
     return (
         <Calendar
             style={[marginStyle(10, 'top'), marginStyle(10, 'bottom')]}
             current={selectedDay}
-            onDayPress={(day) => {
-                clearDayFromCache(selectedDay);
-                clearDayFromCache(day.dateString);
-                setSelectedDay(day.dateString);
-            }}
+            onDayPress={(day) => setSelectedDay(day.dateString)}
             markedDates={currentMarkedDates}
             markingType={'multi-dot'}
             monthFormat={'yyyy MM'}

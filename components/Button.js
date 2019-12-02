02f9ff40af827c47625f08c6a1eb51accd4b3aea
ReallyLiri <reallyliri@gmail.com>
2019-12-02 16:48:50 +0200

    inverted primary button

--- components/Button.js
+++ components/Button.js
@@ -8,17 +8,17 @@ import {hwStyle, borderRadiusStyle, paddingStyle} from "../constants/Styles";
 const StyledTouchable = styled(TouchableOpacity)`
 justify-content: center;
 background-color: ${props => props.disabled ? (props.disabledColor || Colors.gray) : (props.color || Colors.grayLight)};
-border-color: ${Colors.grayMedium};
+border-color: ${props => props.disabled ? Colors.grayMedium : props.borderColor || Colors.grayMedium};
 border-width: ${props => props.noBorder ? 0 : '1px'};
 `;
 
-export default ({style, children, onPress, disabled, noBorder, color, disabledColor, textColor, height, width}) => {
+export default ({style, children, onPress, disabled, noBorder, borderColor, color, disabledColor, disabledTextColor, textColor, height, width}) => {
     return (
         <StyledTouchable
             style={[hwStyle(height || 25, width || 75), borderRadiusStyle(12), paddingStyle(5), style]}
-            onPress={onPress} disabled={disabled} noBorder={noBorder} color={color} disabledColor={disabledColor}
+            onPress={onPress} disabled={disabled} noBorder={noBorder} color={color} disabledColor={disabledColor} borderColor={borderColor}
         >
-            <Text color={textColor || Colors.grayDark} size={9} alignCenter>
+            <Text color={disabled ? (disabledTextColor || Colors.grayDark) : (textColor || Colors.grayDark)} size={9} alignCenter>
                 {children}
             </Text>
         </StyledTouchable>

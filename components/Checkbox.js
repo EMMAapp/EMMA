4c020d54857feb3a7c195d535d6d00a50216d199
ReallyLiri <reallyliri@gmail.com>
2019-12-05 14:45:38 +0200

    checkbox color

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -3,11 +3,11 @@ import React from "react";
 import Colors from "../constants/Colors";
 import Icon from "./Icon";
 
-export default ({value, setValue, disabled, style}) => {
+export default ({value, setValue, disabled, style, color}) => {
     return (
         <TouchableOpacity activeOpacity={1} style={style} onPress={() => setValue(!value)} disabled={disabled}>
             {
-                value ? <Icon name={'checkbox-on'} color={Colors.purple}/> : <Icon name={'checkbox-off'} color={Colors.gray}/>
+                value ? <Icon name={'checkbox-on'} color={color || Colors.purple}/> : <Icon name={'checkbox-off'} color={Colors.gray}/>
             }
         </TouchableOpacity>
     )

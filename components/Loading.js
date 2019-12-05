9f1e3c171608546ff20ccd0f2a487bad016ba055
ReallyLiri <reallyliri@gmail.com>
2019-12-05 14:42:48 +0200

    scroll in modal

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View, ActivityIndicator} from "react-native";
 import localization from "../utils/localization";
+import Colors from "../constants/Colors";
 
 export default () => {
     return <View>
         <Text>{localization('loading')}</Text>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color={Colors.purple} />
     </View>
 }

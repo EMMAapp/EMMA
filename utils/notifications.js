29e8c81ec1e01be2e5d5ad985b42aaaa09f83a3d
ReallyLiri <reallyliri@gmail.com>
2019-12-09 16:12:57 +0200

    sound in notifications

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,23 +1,46 @@
+import {Platform} from 'react-native';
 import {Notifications} from 'expo';
 import * as Permissions from 'expo-permissions';
 import {logError, logInfo} from "./log";
+
+const NOTIFICATIONS_CHANNEL = 'emma';
 
 const createNotification = (title, body) => {
     return {
         title: title,
         body: body,
         ios: {
-            _displayInForeground: true
+            _displayInForeground: true,
+            sound: true
         },
         android: {
+            channelId: NOTIFICATIONS_CHANNEL,
             icon: 'https://cdn3.iconfinder.com/data/icons/web-15/128/RSSvgLink-2-512.png' // TODO
         }
     }
 };
 
+const setup = async () => {
+    if (Platform.OS === 'android') {
+        await Notifications.createChannelAndroidAsync(NOTIFICATIONS_CHANNEL, {
+            name: NOTIFICATIONS_CHANNEL,
+            sound: true,
+            vibrate: true,
+        });
+    }
+    else if (Platform.OS === 'ios') {
+        await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    }
+};
+
+export const testNotification = async () => {
+    await setup();
+    await Notifications.presentLocalNotificationAsync(createNotification("Test", "test!"));
+};
+
 export const setNotification = async (title, body, dateMoment) => {
     try {
-        await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        await setup();
         const id = await Notifications.scheduleLocalNotificationAsync(createNotification(title, body), {time: dateMoment.toDate()});
         logInfo(`Registered notification ${id} at ${dateMoment}`);
         return id;

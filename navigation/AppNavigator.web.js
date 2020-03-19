c7b4d0a011973efd9a1034d2a4ba8c8eb5447586
ReallyLiri <reallyliri@gmail.com>
2020-03-19 18:32:25 +0200

    wip

--- navigation/AppNavigator.web.js
+++
@@ -1,9 +0,0 @@
-import {createBrowserApp} from '@react-navigation/web';
-import {createSwitchNavigator} from 'react-navigation';
-
-import {routeConfigMap, switchConfig} from './Routing';
-
-const switchNavigator = createSwitchNavigator(routeConfigMap, switchConfig);
-switchNavigator.path = '';
-
-export const AppNavigator = createBrowserApp(switchNavigator, {history: 'hash'});

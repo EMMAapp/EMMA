e162cb2c96378c2681757be075e0c0899ef755b0
ReallyLiri <reallyliri@gmail.com>
2020-04-16 15:22:41 +0300

    Fixed keyboard vs tabs in android

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,10 +1,8 @@
-import {Dimensions, Platform} from 'react-native';
+import {Dimensions} from 'react-native';
 
 const width = Dimensions.get("window").width;
 
-const height = Platform.OS === "ios"
-    ? Dimensions.get("window").height
-    : require("react-native-extra-dimensions-android").get("REAL_WINDOW_HEIGHT");
+const height = Dimensions.get("window").height;
 
 export default {
     width,
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Keyboard, Platform} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CalendarTabWrapper from '../screens/Main/Calendar/CalendarTabWrapper';
 import EditEventTab from '../screens/Main/EditEvent/EditEventTab';
 import ProfileTab from '../screens/Main/ProfileTab';
 import Icon from "../components/Icon";
 import Colors from "../constants/Colors";
-import {paddingStyle} from "../constants/Styles";
+import {marginStyle} from "../constants/Styles";
 import JourneyTab from "../screens/Main/JourneyTab";
 import ChartsTab from "../screens/Main/ChartsTab";
 import {CALENDAR, CHARTS, EDIT_EVENT, JOURNEY, PROFILE} from "./Routes";
@@ -19,29 +20,46 @@ const icons = {
 };
 
 const Tab = createBottomTabNavigator();
-export default ({}) => (
-    <Tab.Navigator
-        screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) =>
-                <Icon
-                    color={focused ? Colors.purple : Colors.grayDark}
-                    name={icons[route.name]}
-                />
-        })}
-        options={{
-            headerMode: 'none',
-            navigationOptions: {
-                headerVisible: false,
-            }
-        }}
-        tabBarOptions={{
-            style: {...paddingStyle(10, 'top'), ...paddingStyle(10, 'bottom')},
-            showLabel: false
-        }}>
-        <Tab.Screen name={JOURNEY} component={JourneyTab}/>
-        <Tab.Screen name={CALENDAR} component={CalendarTabWrapper}/>
-        <Tab.Screen name={EDIT_EVENT} component={EditEventTab}/>
-        <Tab.Screen name={CHARTS} component={ChartsTab}/>
-        <Tab.Screen name={PROFILE} component={ProfileTab}/>
-    </Tab.Navigator>
-)
+export default ({}) => {
+    const [keyboardVisible, setKeyboardVisible] = useState(false);
+
+    useEffect(() => {
+        if (Platform.OS !== "android") {
+            return;
+        }
+        const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", setKeyboardVisible(false));
+        const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", setKeyboardVisible(true));
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        }
+    }, []);
+
+    return (
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) =>
+                    <Icon
+                        color={focused ? Colors.purple : Colors.grayDark}
+                        name={icons[route.name]}
+                    />
+            })}
+            options={{
+                headerMode: 'none',
+                navigationOptions: {
+                    headerVisible: false
+                }
+            }}
+            tabBarOptions={{
+                style: keyboardVisible ? {} : {...marginStyle(10, 'top')},
+                showLabel: false,
+                keyboardHidesTabBar: true
+            }}>
+            <Tab.Screen name={JOURNEY} component={JourneyTab}/>
+            <Tab.Screen name={CALENDAR} component={CalendarTabWrapper}/>
+            <Tab.Screen name={EDIT_EVENT} component={EditEventTab}/>
+            <Tab.Screen name={CHARTS} component={ChartsTab}/>
+            <Tab.Screen name={PROFILE} component={ProfileTab}/>
+        </Tab.Navigator>
+    )
+}

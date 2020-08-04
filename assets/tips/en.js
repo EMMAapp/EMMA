4c57babbefcaecef096f97171c0eaf083f7c6137
ReallyLiri <reallyliri@gmail.com>
2020-08-04 13:09:54 +0300

    More en tips

--- assets/tips/en.js
+++ assets/tips/en.js
@@ -9,5 +9,40 @@ export default [
         title: "Injectables",
         text: "\"Injectables?! Sounds scary!\" Moving to the next step in fertility treatments- injectable hormones, is a difficult step. Injecting to oneself may seem like an impossible mission, but with the right technique and some guidance it can become simple. A fertility nurse can help with the first guidance as well as videos created by the company who produces the drug you aim to use.\n" +
             "Another tip from us: the insertion of the needle should be very quick, one shot. But the delivery of the drug when you push the vial should be done slowly, letting the material spread and thus avoiding pain from the drug."
+    },
+    {
+        type: "text",
+        title: "What is IVF",
+        text: "IVF - In Vitro Fertilization - is a procedure used to extract eggs from the female body, fertilize them in a dish and then grow the fertilized egg into embryos for 3 to 5 days. Afterwards the embryos can be transferred back to the uterus or be kept frozen for months and years. The procedure is highly successful in treating certain fertility problems. The success rates of the procedure are 11%-40%, depending on the infertility cause, age of the eggs and other parameters."
+    },
+    {
+        type: "video",
+        title: "How IVF Works",
+        url: "P27waC05Hdk"
+    },
+    {
+        type: "video",
+        title: "IVF Overview",
+        url: "fy61qs_tXTQ"
+    },
+    {
+        type: "text",
+        title: "What is IUI",
+        text: "IUI - Intrauterine Insemination - is a procedure that helps to bring sperm straight into the uterus. The sperm is first washed of its proteins, and afterwards injected through a thin catheter straight into the uterus. Then it's up to nature to do its magic! The sperm will climb up to the fallopian tubes, where it will meet an egg and hopefully fertilize it. Success rates of the procedure are 8%-20% per cycle."
+    },
+    {
+        type: "video",
+        title: "How IUI Works",
+        url: "qCdIiLLF0vw"
+    },
+    {
+        type: "text",
+        title: "What is ICI",
+        text: "ICI - Intra Cervical Insemination - is the procedure of inserting sperm into the cervix of the woman. The sperm used can be either fresh or thawed. It is usually inserted by a catheter. The procedure is the closest to the pathway the sperm goes through during intercourse. The success rates of the procedure are around 5% per cycle. "
+    },
+    {
+        type: "link",
+        title: "Acronyms and Abbreviations",
+        url: "https://www.facebook.com/notes/ttc-fertility-mothers-mothers-to-be/acronyms-abbreviations/395940307084300/"
     }
 ]
